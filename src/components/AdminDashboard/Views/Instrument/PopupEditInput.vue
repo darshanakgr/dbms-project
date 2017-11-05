c85a43cdaa86ae62fc1e92152ff134b3712b61ac
kavinduchamiran <kavindu.chamiran@gmail.com>
2017-11-05 17:41:58 +0530

    Initial Commit

--- src/components/AdminDashboard/Views/Instrument/PopupEditInput.vue
+++ src/components/AdminDashboard/Views/Instrument/PopupEditInput.vue
@@ -1,89 +1,127 @@
 <template>
   <div>
     <transition name="modal">
-        <div class="modal-mask" @click="close" v-show="showEditModal">
-            <div class="modal-container" @click.stop>
-                <div class="modal-header">
-                    <h3>Edit Record</h3>
-                </div>
-                <div class="modal-body">
-                    <label class="form-label">
-                      Instrument Name
-                      <input v-model="editData.instrument_name" class="form-control">
-                    </label>
-                    <label class="form-label">
-                      Purchase Date
-                      <input v-model="editData.purchased_date" type="date" class="form-control">
-                    </label>
-                    <label class="form-label">
-                      Category ID
-                      <input v-model="editData.category_id" class="form-control">
-                    </label>
-                </div>
-                <div class="modal-footer text-right">
-                    <button class="modal-default-button" @click="saveRecord()">
-                        Update record
-                    </button>
-                    <button class="modal-default-button" @click="close()">
-                        Cancel
-                    </button>
-                </div>
-            </div>
+      <div class="modal-mask" @click="close" v-show="showEditModal">
+        <div class="modal-container" @click.stop>
+          <div class="modal-header">
+            <h3>Edit Record</h3>
+          </div>
+          <div class="modal-body">
+            <label class="form-label">
+              Instrument Name
+              <input :value="editData.instrument_name" v-model="dataObject.instrumentName" class="form-control">
+            </label>
+            <label class="form-label">
+              Purchase Date
+              <input :value="editData.purchased_date" type="date" v-model="dataObject.purchasedDate" class="form-control">
+            </label>
+            <label class="form-label">
+              Category ID
+              <input :value="editData.category_id" v-model="dataObject.categoryId" class="form-control">
+            </label>
+          </div>
+          <div class="modal-footer text-right">
+            <button class="modal-default-button" @click="saveRecord()">
+              Update record
+            </button>
+            <button class="modal-default-button" @click="close()">
+              Cancel
+            </button>
+          </div>
         </div>
+      </div>
+    </transition>
+    <transition name="modal">
+      <div class="modal-mask" @click="close" v-show="showDeleteModal">
+        <div class="modal-container" @click.stop>
+          <div class="modal-header">
+            <h3>Delete Record</h3>
+          </div>
+          <div class="modal-body">
+            <label class="form-label">
+              Confirm record deletion?
+            </label>
+          </div>
+          <div class="modal-footer text-right">
+            <button class="modal-default-button" @click="deleteRecord()">
+              Yes
+            </button>
+            <button class="modal-default-button" @click="close()">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
     </transition>
     <div>
-        <button id="show-modal" @click="showEditModal = true">Edit</button>
+      <button id="show-modal" @click="showEditModal = true">Edit</button>
+      <button id="show-delete-modal" @click="showDeleteModal = true">Delete</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    editData: Object
-  },
-  data () {
-    return {
-      showEditModal: false,
-      dataObject: {},
-      title: '',
-      body: ''
-    }
-  },
-  methods: {
-    saveRecord: function () {
-      this.$http.post('http://localhost:3000/addNewInstrument', this.editData).then(function (res) {
-        if (res.ok && res.status === 200) {
-          return alert('Instrument added successfully')
-        }
-        alert('Unable to register this intrument')
-      }).catch(function (err) {
-        console.log(err)
-        alert('Unable to register this intrument')
-      })
+  export default {
+    props: {
+      editData: Object
     },
-    close: function () {
-      this.showEditModal = false
-      this.title = ''
-      this.body = ''
-    }
-  },
-  mounted: function () {
-    document.addEventListener('keydown', (e) => {
-      if (e.keyCode === 27) {
-        this.close()
+    data () {
+      return {
+        showEditModal: false,
+        showDeleteModal: false,
+        dataObject: {},
+        title: '',
+        body: ''
       }
-    })
+    },
+    methods: {
+      saveRecord: function () {
+        this.dataObject.instrumentId = this.editData.instrument_id
+        this.$http.patch('http://localhost:3000/updateInstrument', this.dataObject).then(function (res) {
+          if (res.ok && res.status === 200) {
+            return alert('Instrument updated successfully')
+          }
+          alert('Unable to update this intrument')
+        }).catch(function (err) {
+          console.log(err)
+          alert('Unable to update this intrument')
+        })
+      },
+      deleteRecord: function () {
+        this.dataObject.instrumentId = this.editData.instrument_id
+        this.$http.delete('http://localhost:3000/removeInstrument', this.dataObject).then(function (res) {
+          if (res.ok && res.status === 200) {
+            return alert('Instrument deleted successfully')
+          }
+          alert('Unable to delete this intrument')
+        }).catch(function (err) {
+          console.log(err)
+          alert('Unable to delete this intrument')
+        })
+      },
+      close: function () {
+        this.showEditModal = false
+        this.showDeleteModal = false
+        this.title = ''
+        this.body = ''
+      }
+    },
+    mounted: function () {
+      document.addEventListener('keydown', (e) => {
+        if (e.keyCode === 27) {
+          this.close()
+        }
+      })
+    }
   }
-}
 </script>
 
 <style scoped>
-* {
+  * {
     box-sizing: border-box;
-}
+  }
 
-.modal-mask {
+  .modal-mask {
     position: fixed;
     z-index: 9998;
     top: 0;
@@ -92,9 +130,9 @@ export default {
     height: 100%;
     background-color: rgba(0, 0, 0, .5);
     transition: opacity .3s ease;
-}
+  }
 
-.modal-container {
+  .modal-container {
     width: 300px;
     margin: 40px auto 0;
     padding: 20px 30px;
@@ -103,58 +141,58 @@ export default {
     box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
     transition: all .3s ease;
     font-family: Helvetica, Arial, sans-serif;
-}
+  }
 
-.modal-header h3 {
+  .modal-header h3 {
     margin-top: 0;
     color: #42b983;
-}
+  }
 
-.modal-body {
+  .modal-body {
     margin: 20px 0;
-}
+  }
 
-.text-right {
+  .text-right {
     text-align: right;
-}
+  }
 
-.form-label {
+  .form-label {
     display: block;
     margin-bottom: 1em;
-}
+  }
 
-.form-label > .form-control {
+  .form-label > .form-control {
     margin-top: 0.5em;
-}
+  }
 
-.form-control {
+  .form-control {
     display: block;
     width: 100%;
     padding: 0.5em 1em;
     line-height: 1.5;
     border: 1px solid #ddd;
-}
+  }
 
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
+  /*
+   * The following styles are auto-applied to elements with
+   * transition="modal" when their visibility is toggled
+   * by Vue.js.
+   *
+   * You can easily play with the modal transition by editing
+   * these styles.
+   */
 
-.modal-enter {
-  opacity: 0;
-}
+  .modal-enter {
+    opacity: 0;
+  }
 
-.modal-leave-active {
-  opacity: 0;
-}
+  .modal-leave-active {
+    opacity: 0;
+  }
 
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
+  .modal-enter .modal-container,
+  .modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
 </style>
