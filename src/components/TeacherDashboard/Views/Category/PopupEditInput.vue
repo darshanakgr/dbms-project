df263378ddf0316f636991e9db6457b6a806c523
kavinduchamiran <kavindu.chamiran@gmail.com>
2017-11-09 07:17:23 +0530

    morning push

---
+++ src/components/TeacherDashboard/Views/Category/PopupEditInput.vue
@@ -0,0 +1,186 @@
+<template>
+  <div>
+    <transition name="modal">
+      <div class="modal-mask" @click="close" v-show="showEditModal">
+        <div class="modal-container" @click.stop>
+          <div class="modal-header">
+            <h3>Edit Record</h3>
+          </div>
+          <div class="modal-body">
+            <label class="form-label">
+              Edit Category
+              <input :value="editData.instrument_type" v-model="dataObject.instrumentType" name="instrumentType" v-validate="'required|alpha'" class="form-control">
+            </label>
+            <span v-show="errors.has('instrumentType')" style="color:red">Invalid instrument type</span>
+          </div>
+          <div class="modal-footer text-right">
+            <button class="modal-default-button" @click="saveRecord()">
+              Update record
+            </button>
+            <button class="modal-default-button" @click="close()">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <transition name="modal">
+      <div class="modal-mask" @click="close" v-show="showDeleteModal">
+        <div class="modal-container" @click.stop>
+          <div class="modal-header">
+            <h3>Delete Record</h3>
+          </div>
+          <div class="modal-body">
+            <label class="form-label">
+              Confirm record deletion?
+            </label>
+          </div>
+          <div class="modal-footer text-right">
+            <button class="modal-default-button" @click="deleteRecord()">
+              Yes
+            </button>
+            <button class="modal-default-button" @click="close()">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <div>
+      <button id="show-modal" @click="showEditModal = true">Edit</button>
+      <button id="show-delete-modal" @click="showDeleteModal = true">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      editData: Object
+    },
+    data () {
+      return {
+        showEditModal: false,
+        showDeleteModal: false,
+        dataObject: {},
+        title: '',
+        body: ''
+      }
+    },
+    methods: {
+      saveRecord: function () {
+        this.$validator.validateAll().then((result) => {
+          if (result) {
+            // alert(result)
+            this.$http.patch('http://localhost:3000/updateCategory', this.dataObject).then(function (res) {
+              if (res.ok && res.status === 200) {
+                return alert('Category updated successfully')
+              }
+              alert('Unable to update this category')
+            }).catch(function (err) {
+              console.log(err)
+              alert('Unable to update this category')
+            })
+            this.close()
+          }
+        })
+      },
+      deleteRecord: function () {
+        this.$http.post('http://localhost:3000/removeCategory', this.dataObject).then(function (res) {
+          if (res.ok && res.status === 200) {
+            return alert('category deleted successfully')
+          }
+          alert('Unable to delete this category-')
+        }).catch(function (err) {
+          console.log(err)
+          alert('Unable to delete this category--')
+        })
+        this.close()
+      },
+      close: function () {
+        this.showEditModal = false
+        this.showDeleteModal = false
+        this.title = ''
+        this.body = ''
+      }
+    },
+    mounted: function () {
+      this.dataObject.categoryId = this.editData.category_id
+      this.dataObject.instrumentType = this.editData.instrument_type
+      document.addEventListener('keydown', (e) => {
+        if (e.keyCode === 27) {
+          this.close()
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    transition: opacity .3s ease;
+  }
+  .modal-container {
+    width: 300px;
+    margin: 40px auto 0;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    transition: all .3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  .modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+  }
+  .modal-body {
+    margin: 20px 0;
+  }
+  .text-right {
+    text-align: right;
+  }
+  .form-label {
+    display: block;
+    margin-bottom: 1em;
+  }
+  .form-label > .form-control {
+    margin-top: 0.5em;
+  }
+  .form-control {
+    display: block;
+    width: 100%;
+    padding: 0.5em 1em;
+    line-height: 1.5;
+    border: 1px solid #ddd;
+  }
+  /*
+   * The following styles are auto-applied to elements with
+   * transition="modal" when their visibility is toggled
+   * by Vue.js.
+   *
+   * You can easily play with the modal transition by editing
+   * these styles.
+   */
+  .modal-enter {
+    opacity: 0;
+  }
+  .modal-leave-active {
+    opacity: 0;
+  }
+  .modal-enter .modal-container,
+  .modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+</style>
---
+++ src/components/TeacherDashboard/Views/Play.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <paper-table :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns"
+                     :columnNames="table1.columnNames">
+
+        </paper-table>
+      </div>
+    </div>
+    <div align="center">
+      <button @click="pageNo-=10">Previous page</button>
+      Page {{this.pageNo / 10 + 1}}
+      <button @click="pageNo+=10">Next page</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PaperTable from './Play/PaperTable.vue'
+
+  const tableColumnNames = ['Student ID', 'Instrument ID', 'Options']
+  /* Give here the names of the attributes */
+  const tableColumns = ['student_id', 'instrument_id', 'options']
+  /* Give here the column names as they are in the table */
+  const tableData = []
+  /* table data will be loaded upon created(), code is below */
+
+  export default {
+    components: {
+      PaperTable
+    },
+    data () {
+      return {
+        table1: {
+          title: 'Plays',
+          subTitle: 'All the instruments that are played by students in this school are listed below.',
+          columns: [...tableColumns],
+          columnNames: [...tableColumnNames],
+          data: [...tableData]
+        },
+        pageNo: 0,
+        tableSize: 0
+      }
+    },
+    created () {
+      this.$http.get('http://localhost:3000/getAllPlays').then(function (data) {   /* get address here */
+        this.tableData = data.body.slice(0, 10)
+        /* retrive rows, 10 by 10 */
+        this.table1.data = [...this.tableData]
+      })
+    },
+    updated () {
+      if (this.pageNo < 0) {
+        this.pageNo = 0
+      }
+      this.$http.get('http://localhost:3000/getAllPlays').then(function (data) {   /* get address here */
+        this.tableData = data.body.slice(this.pageNo, this.pageNo + 10)
+        /* update the row range when buttons are clicked */
+        if (this.tableData.length === 0) {
+          this.pageNo -= 10
+        }
+        this.table1.data = [...this.tableData]
+      })
+    }
+  }
+</script>
+<style>
+
+</style>
